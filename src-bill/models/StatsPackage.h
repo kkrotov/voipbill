1500264adccfb6b593fb6caa9ca6a4b535b88067
ivanov <e45456@yandex.ru>
2015-12-15 01:29:53 +0300

    Вывод отладочной информации по пакету

--- src-bill/models/StatsPackage.h
+++ src-bill/models/StatsPackage.h
@@ -12,4 +12,18 @@ struct StatsPackage {
     int paid_seconds;
     long long int min_call_id;
     long long int max_call_id;
+
+    void dump(stringstream &trace) {
+        trace << "(";
+        trace << "id: " << id << ", ";
+        trace << "package_id: " << package_id << ", ";
+        trace << "activation_dt: " << string_time(activation_dt) << ", ";
+        trace << "expire_dt: " << string_time(expire_dt) << ", ";
+        trace << "used_seconds: " << used_seconds << ", ";
+        trace << "used_credit: " << used_credit << ", ";
+        trace << "paid_seconds: " << paid_seconds << ", ";
+        trace << "min_call_id: " << min_call_id << ", ";
+        trace << "max_call_id: " << max_call_id << ", ";
+        trace << ")";
+    }
 };
