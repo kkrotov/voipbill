4280179d04eae15a096836ab06ee8e921b43023f
avesus <avesus8@gmail.com>
2016-02-29 17:11:57 +0300

    Синхронизация курсов валют центр->регион
    BIL-1268

--- src-bill/threads/ThreadSync.cpp
+++ src-bill/threads/ThreadSync.cpp
@@ -19,6 +19,7 @@
 #include "../sync/PullTariffLog.h"
 #include "../sync/PullStatPrefixlist.h"
 #include "../sync/PullStatDestinationPrefixlists.h"
+#include "../sync/PullCurrencyRate.h"
 
 #include "../sync/PullServer.h"
 #include "../sync/PullAirp.h"
@@ -70,6 +71,7 @@ ThreadSync::ThreadSync() {
     manager->add(new PullLogTariff());
     manager->add(new PullStatPrefixlist());
     manager->add(new PullStatDestinationPrefixlists());
+    manager->add(new PullCurrencyRate());
 
     manager->add(new PullServer());
     manager->add(new PullAirp());
