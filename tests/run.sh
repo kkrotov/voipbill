e56b43c0fbfa8b46d01c86d25e0b1aabc1829202
Konstantin Krotov <k.krotov@mcn.ru>
2016-06-29 12:48:11 +0300

    Изменены таймауты в скриптах запуска тестов

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# run.sh [аргумент]
+#  агумент = waitsync - скрипт ждет отсутствия команд на синхронизацию в табличке queue
+# либо агумент = timeout в секундах, по умолчанию 120с
+# 
+
+if [ "$1" > 0 ]; then
+  timeout="$1";
+else
+  timeout=120
+fi
+
+
 THIS=`readlink -f "${BASH_SOURCE[0]}"`
 DIR=`dirname "${THIS}"`
 
@@ -13,8 +25,26 @@ do
   (./app_bill -c app_bill${region_id}.conf -p /var/run/app_bill${region_id}.pid; echo "Daemon ${region_id} finished with exit code $?") &
   echo "Daemon ${region_id} runned with pid $!"
 
+
+if [ "$1" = "waitsync" ]; then
+
+  while true
+  do
+    echo "проверяем наличие компанд синхронизации для региона $region_id"
+    res=`/usr/pgsql-9.4/bin/psql -t -U postgres nispd_test -c "select count(*) from event.queue where server_id=${region_id}"`
+    if [ "$res" -eq 0 ]; then
+       break            
+    fi     
+    sleep 5s
+    echo "sleep 5sec : Команд в очереди для сервера  $region_id : $res"
+  done
+
+
+else
   # Каждому процессу даём поработать две минуты, иначе текущие версии демона падают в разных местах
-  sleep 120s
+  sleep  "$timeout"s
+fi
+
 
 done
 
