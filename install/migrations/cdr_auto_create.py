f1f2a075920e398c6e72a811c843b21490999f5d
Konstantin Krotov <k.krotov@mcn.ru>
2016-08-11 16:47:31 +0300

    Создан триггер для автоматического создания таблиц calls_raw В скрипт миграции добавлена забытая функция insert_cdr_unfinished Добавлен скрипт модификации триггерной ф-и calls_raw.tr_partitioning ./install/sql/1-calls_raw_autocreate.sql для автоматического создания таблиц calls_raw.calls_rawYYYYMM

--- install/migrations/cdr_auto_create.py
+++ install/migrations/cdr_auto_create.py
@@ -145,6 +145,86 @@ ALTER FUNCTION calls_cdr.tr_partitioning_cdr_unfinished()
   OWNER TO postgres;
     ''')
 
+    curReg.execute('''
+CREATE OR REPLACE FUNCTION calls_raw.tr_partitioning()
+  RETURNS trigger AS
+$BODY$
+declare
+	relname varchar;
+	rel_exists text;
+	suffix varchar;
+	this_mon timestamp;
+	next_mon timestamp;
+begin
+	suffix := to_char(new.connect_time, 'YYYYMM');
+	--raise notice '%', suffix;
+	relname := 'calls_raw.calls_raw_' || suffix;
+	--raise notice '%', relname;
+	EXECUTE 'SELECT to_regclass('|| quote_literal(relname) ||');' INTO rel_exists;
+	
+	IF rel_exists IS NULL OR rel_exists = ''
+	THEN
+		EXECUTE 'select date_trunc(''month'', TIMESTAMP ' || quote_literal(new.connect_time) || ' );' INTO this_mon;
+		EXECUTE 'select date_trunc(''month'', TIMESTAMP ' || quote_literal(new.connect_time) || ' + INTERVAL ''1 MON'');' INTO next_mon;
+			
+		EXECUTE 'CREATE TABLE calls_raw.calls_raw_' || suffix || 
+			' (CONSTRAINT calls_raw_' || suffix || '_pkey PRIMARY KEY (id),' ||
+			' CONSTRAINT calls_raw_' || suffix || '_connect_time_check CHECK (' || 
+			'connect_time >= ' || quote_literal(this_mon) || '::timestamp without time zone AND ' || 
+			'connect_time < ' || quote_literal(next_mon) || '::timestamp without time zone)' || 
+			') INHERITS (calls_raw.calls_raw) WITH (OIDS=FALSE)';
+
+		EXECUTE format('CREATE INDEX calls_raw_' || suffix || '_cdr_id ON calls_raw.calls_raw_' || suffix || ' USING btree (cdr_id)');
+		EXECUTE 'CREATE INDEX calls_raw_' || suffix || '_connect_time ON calls_raw.calls_raw_' || suffix || ' USING btree (connect_time)';
+
+		EXECUTE 'ALTER TABLE ' || relname || ' OWNER TO postgres';
+		EXECUTE 'GRANT ALL ON TABLE ' || relname || ' TO postgres';
+		EXECUTE 'GRANT SELECT, INSERT, DELETE ON TABLE ' || relname || ' TO g_bill_daemon_local';
+		EXECUTE 'GRANT SELECT ON TABLE ' || relname || ' TO g_readonly';
+	END IF;
+
+        EXECUTE format('INSERT INTO ' || relname || ' SELECT ($1).*') USING NEW;
+	return null;        
+end;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION calls_raw.tr_partitioning()
+  OWNER TO postgres;
+    ''')
+
+    curReg.execute('''
+CREATE OR REPLACE FUNCTION insert_cdr_unfinished(
+    p_call_id bigint,
+    setup_time character varying,
+    dst_route character varying,
+    releasing_party character varying,
+    release_timestamp character varying,
+    disconnect_cause smallint)
+  RETURNS void AS
+$BODY$
+        declare 
+            p_hash uuid;
+        begin
+
+            p_hash = calls_cdr.get_hash(p_call_id, setup_time::timestamp);
+   
+            insert into calls_cdr.cdr_unfinished (
+                call_id, setup_time, hash, dst_route, releasing_party, release_timestamp, disconnect_cause
+            ) values (
+                p_call_id, setup_time::timestamp, p_hash, dst_route, releasing_party, release_timestamp::timestamp, disconnect_cause
+            );
+
+            delete from calls_cdr.start where call_id = p_call_id;
+    
+        end;
+        $BODY$
+  LANGUAGE plpgsql VOLATILE SECURITY DEFINER
+  COST 100;
+ALTER FUNCTION insert_cdr_unfinished(bigint, character varying, character varying, character varying, character varying, smallint)
+  OWNER TO postgres;
+    ''')
+
     regConn.commit()
     regConn.close()
 
