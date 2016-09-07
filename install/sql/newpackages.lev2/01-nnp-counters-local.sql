1613c4f172e812a2eabb08fa2ffb0ac5013aca12
nedlosster <nedlosster@gmail.com>
2016-09-07 12:15:08 +0300

    Сделал sql код для создания таблицы счетчиков новых пакетов в региональной и центральных базах, небольшые корректировки в скриптах формирования локального тестового окружения БД.

---
+++ install/sql/newpackages.lev2/01-nnp-counters-local.sql
@@ -0,0 +1,59 @@
+DROP TABLE IF EXISTS  billing.stats_nnp_package_minute;
+
+CREATE TABLE billing.stats_nnp_package_minute
+(
+  nnp_account_tariff_light_id integer NOT NULL,
+  nnp_package_minute_id  integer NOT NULL,
+  activate_from timestamp(0) without time zone NOT NULL,
+  deactivate_from timestamp(0) without time zone NOT NULL,
+  used_seconds integer NOT NULL,
+  paid_seconds integer NOT NULL,
+  used_credit double precision NOT NULL,
+  min_call_id bigint NOT NULL,
+  max_call_id bigint NOT NULL,
+  CONSTRAINT stats_nnp_package_minute_pkey PRIMARY KEY (nnp_account_tariff_light_id,nnp_package_minute_id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE billing.stats_nnp_package_minute
+  OWNER TO postgres;
+GRANT ALL ON TABLE billing.stats_nnp_package_minute TO postgres;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE billing.stats_nnp_package_minute TO g_bill_daemon_local;
+GRANT SELECT ON TABLE billing.stats_nnp_package_minute TO g_readonly;
+-- 
+-- -- Index: billing.idx_stats_package__max_call_id
+-- 
+
+
+CREATE INDEX idx_stats_nnp_package_minute_max_call_id
+  ON billing.stats_nnp_package_minute
+  USING btree
+  (max_call_id);
+
+-- Rule: stats_package_rl ON billing.stats_package
+
+-- DROP RULE stats_package_rl ON billing.stats_package;
+
+CREATE OR REPLACE RULE stats_nnp_package_minute_rl AS
+    ON INSERT TO billing.stats_nnp_package_minute
+   WHERE (EXISTS ( SELECT x.nnp_account_tariff_light_id,x.nnp_package_minute_id
+           FROM billing.stats_nnp_package_minute x
+          WHERE x.nnp_account_tariff_light_id = new.nnp_account_tariff_light_id AND x.nnp_package_minute_id=new.nnp_package_minute_id)) DO INSTEAD  
+          
+          UPDATE billing.stats_nnp_package_minute
+          
+          SET   activate_from = new.activate_from , 
+                deactivate_from = new.deactivate_from, 
+                used_seconds = new.used_seconds, 
+                paid_seconds = new.paid_seconds, 
+                used_credit = new.used_credit, 
+                min_call_id = new.min_call_id, 
+                max_call_id = new.max_call_id
+  WHERE stats_nnp_package_minute.nnp_account_tariff_light_id = new.nnp_account_tariff_light_id AND stats_nnp_package_minute.nnp_package_minute_id = new.nnp_package_minute_id ;
+
+
+ALTER TABLE nnp.account_tariff_light ALTER COLUMN activate_from TYPE timestamp without time zone;
+ALTER TABLE nnp.account_tariff_light ALTER COLUMN deactivate_from TYPE timestamp without time zone;
+
