565d0b219e24c826a7d3ff5ab7702a4b171bf808
Konstantin Krotov <k.krotov@mcn.ru>
2016-09-23 15:15:59 +0300

    Добавлена возможность задания момента времени, в который актуально log-сообщение. Используется при записи в graylog сообщений об обработанных записях из получаемых спустя час cdr-файлов

--- src-bill/classes/Log.h
+++ src-bill/classes/Log.h
@@ -7,13 +7,13 @@ using namespace std;
 
 class Log {
 public:
-    static void info(const string &text);
-    static void info(pLogMessage message);
-    static void error(const string &text);
-    static void notice(const string &text);
-    static void notice(pLogMessage message);
-    static void warning(const string &text);
-    static void debug(const string &text);
+    static void info(const string &text, time_t log_time=0);
+    static void info(pLogMessage message, time_t log_time=0);
+    static void error(const string &text, time_t log_time=0);
+    static void notice(const string &text, time_t log_time=0);
+    static void notice(pLogMessage message, time_t log_time=0);
+    static void warning(const string &text, time_t log_time=0);
+    static void debug(const string &text, time_t log_time=0);
 
     static void exception(Exception &e);
 
