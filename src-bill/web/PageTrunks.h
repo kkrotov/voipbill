50756923c9fad6ca2e5e9b59c4dabd6fae3810b8
Konstantin Krotov <k.krotov@mcn.ru>
2016-10-04 10:45:40 +0300

    Добавлена созможность сортировки выводимой таблицы транков по столбцам trunk_name,capacity,incoming count,outgoing count,total count,load

--- src-bill/web/PageTrunks.h
+++ src-bill/web/PageTrunks.h
@@ -8,7 +8,8 @@ class TrunkIoParams {
 public:
     string trunk_name;
     int num_of_incoming,
-        num_of_outgoing;
+        num_of_outgoing,
+        capacity;
     double incoming_cost,
         outgoing_cost;
 
@@ -30,15 +31,16 @@ public:
             outgoing_cost = cost;
         }
     }
+    void setCapacity(int capacity) {this->capacity=capacity;};
 };
 
 class ActiveTrunks {
 
-    vector<TrunkIoParams> activeTrunks;
+    vector<TrunkIoParams> trunkIoParams;
 
     void add (string name, bool incoming, double cost) {
 
-        for (TrunkIoParams &trunk: activeTrunks) {
+        for (TrunkIoParams &trunk: trunkIoParams) {
 
             if (trunk.trunk_name.compare(name)==0) {
 
@@ -55,7 +57,7 @@ class ActiveTrunks {
                 return;
             }
         }
-        activeTrunks.push_back(TrunkIoParams(name, incoming, cost));
+        trunkIoParams.push_back(TrunkIoParams(name, incoming, cost));
     }
 
 public:
@@ -78,8 +80,38 @@ public:
         }
     }
 
-    int count() { return activeTrunks.size(); }
-    TrunkIoParams at(int i) { return activeTrunks.at(i); }
+    int count() { return trunkIoParams.size(); }
+
+    void sort(int sort_column, bool order) {
+
+        if (sort_column==0)
+            std::sort(trunkIoParams.begin(), trunkIoParams.end(), [](TrunkIoParams a, TrunkIoParams b) {
+                return b.trunk_name < a.trunk_name;
+        });
+        if (sort_column==1)
+            std::sort(trunkIoParams.begin(), trunkIoParams.end(), [](TrunkIoParams a, TrunkIoParams b) {
+                return b.capacity < a.capacity;
+            });
+        if (sort_column==2)
+            std::sort(trunkIoParams.begin(), trunkIoParams.end(), [](TrunkIoParams a, TrunkIoParams b) {
+                return b.num_of_incoming < a.num_of_incoming;
+            });
+        if (sort_column==4)
+            std::sort(trunkIoParams.begin(), trunkIoParams.end(), [](TrunkIoParams a, TrunkIoParams b) {
+                return b.num_of_outgoing < a.num_of_outgoing;
+            });
+        if (sort_column==6)
+            std::sort(trunkIoParams.begin(), trunkIoParams.end(), [](TrunkIoParams a, TrunkIoParams b) {
+                return (b.num_of_outgoing+b.num_of_incoming) < (a.num_of_outgoing+a.num_of_incoming);
+            });
+        if (sort_column==8)
+            std::sort(trunkIoParams.begin(), trunkIoParams.end(), [](TrunkIoParams a, TrunkIoParams b) {
+                int a_load = ((a.num_of_incoming+a.num_of_outgoing)*100/a.capacity);
+                int b_load = ((b.num_of_incoming+b.num_of_outgoing)*100/b.capacity);
+                return a_load < b_load;
+            });
+    }
+    TrunkIoParams at(int i) { return trunkIoParams.at(i); }
 };
 
 class PageTrunks : public BasePage {
@@ -92,10 +124,10 @@ public:
     }
     void render(std::stringstream &html, map<string, string> &parameters) {
 
+        renderHeader(html);
+
         if (!ready())
             return;
-
-        renderHeader(html);
 
         shared_ptr<CurrentCdrList> cdrList = repository.currentCalls->currentCdr.get();
         if (cdrList == nullptr) {
@@ -123,22 +155,31 @@ public:
         bool fullMode = calls->size() == cdrList->size() * 2;
         html << "<table width=100% border=0 cellspacing=0>\n";
         html << "<tr>"
-                "<th nowrap>trunk_name</th>"
-                "<th nowrap>capacity</th>"
-                "<th nowrap>incoming count</th>";
+                "<th nowrap><a href='/trunks?sort=0&order=0'>trunk_name</a></th>"
+                "<th nowrap><a href='/trunks?sort=1&order=0'>capacity</a></th>"
+                "<th nowrap><a href='/trunks?sort=2&order=0'>incoming count</a></th>";
         if (fullMode)
             html << "<th nowrap>incoming cost</th>";
 
-        html << "<th nowrap>outgoing count</th>";
+        html << "<th nowrap><a href='/trunks?sort=4&order=0'>outgoing count</a></th>";
         if (fullMode)
             html << "<th nowrap>outgoing cost</th>";
 
-        html << "<th nowrap>total count</th>";
+        html << "<th nowrap><a href='/trunks?sort=6&order=0'>total count</a></th>";
         if (fullMode)
             html << "<th nowrap>total cost</th>";
 
-        html << "<th nowrap>load</th>";
-        html << "</tr>";;
+        html << "<th nowrap><a href='/trunks?sort=8&order=0'>load</a></th>";
+        html << "</tr>";
+
+        string sort_column = parameters["sort"];
+        int sort_col_num = 0;
+        if (sort_column.size()>0)
+            sort_col_num = std::stoi(sort_column);
+
+        string order = parameters["order"];
+        bool asc = order.compare("1");
+        activeTrunks.sort(sort_col_num, asc);
 
         for (size_t i = 0; i < activeTrunks.count(); i++) {
 
@@ -148,6 +189,8 @@ public:
 
             Trunk *trunk = repository.getTrunkByName(trunkParam.trunk_name.c_str());
             int capacity = trunk!= nullptr? trunk->capacity:0;
+            trunkParam.setCapacity(capacity);
+
             html << "<td nowrap>" << capacity << "</td>\n";
 
             if (trunkParam.num_of_incoming>0)
