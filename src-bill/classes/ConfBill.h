2dd99190611d3f85151c0e18db872cac502d089a
Konstantin Krotov <k.krotov@mcn.ru>
2016-10-07 14:22:15 +0300

    В файл app_bill.conf добавлен ключевой параметр calls_raw_sync_delay, который в сек. задаёт паузу между запусками метола run потока синхронизации таблицы вызовов calls_raw с центральным сервером

--- src-bill/classes/ConfBill.h
+++ src-bill/classes/ConfBill.h
@@ -35,6 +35,7 @@ public:
     std::string cdr_ftp_dir;
     std::string cdr_nasip;
     int cdr_parcer_interval;
+    int calls_raw_sync_delay;
 
 protected:
     virtual bool parse_config_variables(boost::property_tree::ptree &pt);
