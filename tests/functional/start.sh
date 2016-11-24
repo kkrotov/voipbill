cd47f56c4acedbf2ac602829fbd7de847f213f6d
markgrin <lew.ka@hotmail.com>
2016-11-24 12:51:14 +0300

    Добавление xml отчетов функционального и юнит тестирований

--- tests/functional/start.sh
+++ tests/functional/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo 'Запускаем functional-тесты'
-$DIR/functional/ftest
+$DIR/functional/ftest > ../TEST-functional.XML
 [[ $? -ne 0 ]] && exit
+
--- tests/unit/start.sh
+++ tests/unit/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo 'Запускаеи unit-тесты'
-$DIR/unit/utest
+$DIR/unit/utest > ../TEST_unit.xml
 [[ $? -ne 0 ]] && exit
 
