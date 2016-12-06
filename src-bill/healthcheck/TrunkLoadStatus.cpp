bc236833a21c18fb647d2004dbda373cb55a91d1
Konstantin Krotov <k.krotov@mcn.ru>
2016-12-06 13:06:09 +0300

    В сообщение процедуры проверки загрузки транков TrunkLoadStatus выводятся все транки, у которых загрузка != STATUS_OK

--- src-bill/healthcheck/TrunkLoadStatus.cpp
+++ src-bill/healthcheck/TrunkLoadStatus.cpp
@@ -15,18 +15,30 @@ SystemStatus TrunkLoadStatus::getStatus() {
     if (ready()) {
 
         ActiveTrunks activeTrunks(repository);
-        std::string trunkName;
-        int maxload = activeTrunks.maxLoad(trunkName);
-        checkStatus (std::vector<std::pair<time_t, HealthStatus>> {
+        vector<pair<int,string>> trunkLoad = activeTrunks.getLoad();
+        std::sort(trunkLoad.begin(), trunkLoad.end(), [](pair<int,string> a, pair<int,string> b) {
 
-                std::pair<time_t, HealthStatus>(app().conf.trunk_max_load[0],HealthStatus::STATUS_OK),
-                std::pair<time_t, HealthStatus>(app().conf.trunk_max_load[1],HealthStatus::STATUS_WARNING),
-                std::pair<time_t, HealthStatus>(app().conf.trunk_max_load[2],HealthStatus::STATUS_ERROR)
-        }, maxload);
-        healthStatus.itemValue = to_string(maxload);
-        healthStatus.statusMessage = "Maximum trunk load is "+to_string(maxload)+"%";
-        if (maxload>0)
-            healthStatus.statusMessage += ", Trunk name: "+trunkName;
+            return b.first>a.first; // asc
+        });
+        healthStatus.statusMessage = "";
+        healthStatus.statusId = HealthStatus::STATUS_UNKNOWN;
+        for (auto load : trunkLoad) {
+
+            checkStatus (std::vector<std::pair<time_t, HealthStatus>> {
+
+                    std::pair<time_t, HealthStatus>(app().conf.trunk_max_load[0],HealthStatus::STATUS_OK),
+                    std::pair<time_t, HealthStatus>(app().conf.trunk_max_load[1],HealthStatus::STATUS_WARNING),
+                    std::pair<time_t, HealthStatus>(app().conf.trunk_max_load[2],HealthStatus::STATUS_ERROR)
+            }, load.first);
+            if (healthStatus.statusId == HealthStatus::STATUS_OK)
+                continue;
+
+            healthStatus.itemValue = to_string(load.first);
+            if (healthStatus.statusMessage.size()>0)
+                healthStatus.statusMessage += "; ";
+
+            healthStatus.statusMessage += "Trunk name: "+load.second+", Trunk load: "+to_string(load.first)+"%";
+        }
     }
     return healthStatus;
 }
