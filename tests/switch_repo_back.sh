25195acdb2822d6ab048bde47a6064396bf160cc
markgrin <lew.ka@hotmail.com>
2017-01-24 18:59:09 +0300

    Сохранение папки tests/comparative между переключений веток

--- tests/switch_repo_back.sh
+++ tests/switch_repo_back.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -r -f tests/comparative
+
 git checkout new
 
 mv app_bill_new app_bill
--- tests/switch_repo_to_tested.sh
+++ tests/switch_repo_to_tested.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mv app_bill app_bill_new
+mv tests/comparative tests/comparative_save
 
 # Именуем ветку текущего теста, чтобы вернуться в неё из скрипта
 git checkout -b new
@@ -11,3 +12,4 @@ git checkout -b new
 git pull origin released:released
 git checkout released
 
+mv tests/comparative_save tests/comparative
