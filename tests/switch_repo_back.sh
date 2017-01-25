f289204ee3ee7e1d4352792484be9d0c3e60f7f9
markgrin <lew.ka@hotmail.com>
2017-01-25 17:46:22 +0300

    Убрано временное решение

--- tests/switch_repo_back.sh
+++ tests/switch_repo_back.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-rm -r -f tests/comparative
-
 git checkout new
 
 mv app_bill_new app_bill
-
--- tests/switch_repo_to_tested.sh
+++ tests/switch_repo_to_tested.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 mv app_bill app_bill_new
-mv tests/comparative tests/comparative_save
 
 # Именуем ветку текущего теста, чтобы вернуться в неё из скрипта
 git checkout -b new
@@ -12,4 +11,3 @@ git checkout -b new
 git pull origin released:released
 git checkout released
 
-mv tests/comparative_save tests/comparative
